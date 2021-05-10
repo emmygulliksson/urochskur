a915790506ff08780f6a7cbdbfc2ee8631135bad
ella <id18esg@cs.umu.se>
2021-05-10 10:32:01 +0200

    new file

---
+++ src/Components/Data/forecast5DaysWeather.js
@@ -0,0 +1,21 @@
+import react from 'react';
+import axios from 'axios';
+
+const baseURL= 'http://api.openweathermap.org/data/2.5/forecast?';
+const apiKey='a70472aab8b16f40783f2271ba271199';
+
+export const forecast5DaysWeather= async (cityName)=>{
+    try{
+        const {data}= await axios.get(baseURL + `q=${cityName}&appid=${apiKey}`);
+        //const {data}= await axios.get(baseURL + 'q=London&appid=a70472aab8b16f40783f2271ba271199');
+
+        console.log(data);
+        return data;
+
+    }catch(error){
+        console.log(error.message);
+        throw error;
+    }
+}
+
+export default forecast5DaysWeather;
