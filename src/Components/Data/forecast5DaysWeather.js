bbfb9cb4af0b98ae17fc8d01904253cbf83ceabb
emmygulliksson <id18egn@cs.umu.se>
2021-05-11 18:08:47 +0200

    new key

--- src/Components/Data/forecast5DaysWeather.js
+++ src/Components/Data/forecast5DaysWeather.js
@@ -2,7 +2,7 @@ import react from 'react';
 import axios from 'axios';
 
 const baseURL= 'http://api.openweathermap.org/data/2.5/forecast?';
-const apiKey='a70472aab8b16f40783f2271ba271199';
+const apiKey='3434a41e4751b5969309b9363a302a9b';
 
 export const getWeatherData= async (cityName)=>{
     try{
